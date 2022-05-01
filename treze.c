613472b9fd38ef0d47627e071bfb4aca23875b2e
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-05-01 15:41:11 -0300

    Update treze.c

--- treze.c
+++ treze.c
@@ -1,38 +1,42 @@
-//Construa um programa que dado quatro valores, A, B, C e D, o algoritmo imprima o maior e o menor valor.
-//resolução mais brute force possivel
+//) Construa um programa que dado quatro valores, A, B, C e D, o algoritmo imprima o maior e o menor valor.
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
+{   float a, b, c, d, maior=0, menor=0;
+    scanf("%f %f %f %f", &a, &b, &c, &d);
+ 
+    menor=a;
+ 
+    if(a>maior)
+        maior=a;
+    else if (a<menor)
+        menor=a;
 
-{   float a=1, b=2, c=3, d=4, maior, menor;
+ 
+    if(b>maior)
+        maior=b;
+    else if (b<menor)
+        menor=b;
 
+ 
+    if(c>maior)
+        maior=c;
+    else if (c<menor)
+        menor=c;
 
-    if ((a > b)&&(a>c)&&(a>d)){
-        maior= a;
-    }
-    else if ((b > a)&&(b>c)&&(b>d)){
-        maior= b;
-    }
-    else if ((c > a)&&(c>b)&&(c>d)){
-        maior= c;
-    }
-    else if ((d > a)&&(d>b)&&(d>c)){
-        maior= d;
-    }
+ 
+    if(d>maior)
+        maior=d;
+    else if (d<menor)
+        menor=d;
 
+ 
+    printf("%f %f", maior, menor);
+    return 0;
+}
 
-
-    if ((a < b)&&(a<c)&&(a<d)){
-        menor= a;
-    }
-    else if ((b < a)&&(b<c)&&(b<d)){
-        menor= b;
-    }
-    else if ((c < a)&&(c<b)&&(c<d)){
-        menor= c;
-    }
     else if ((d < a)&&(d<b)&&(d<c)){
         menor= d;
     }
