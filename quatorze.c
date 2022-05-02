c3584284d31e5d00e4640eb76ef5141300def753
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-05-02 02:50:31 -0300

    Create quatorze.c

---
+++ quatorze.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int a, b, c;
+    scanf ("%d %d %d", &a, &b, &c);
+    if(c>a){
+        int tmp;
+        tmp=a;
+        a=c;
+        c=a;
+    }
+    if(b>a){
+        int tmp;
+        tmp=a;
+        a=b;
+        b=a;
+    }
+    if(c>b){
+        int tmp;
+        tmp=b;
+        c=b;
+        b=c;
+    }
+    printf("%d %d %d ", c , b , a );
+
+    return 0;
+}
